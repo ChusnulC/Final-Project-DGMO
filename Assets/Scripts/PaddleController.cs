0f77f0db72e087b38150ddf49eff4d36ff3cd4fe
Chusnul Chotimah <chusnulchotimah888@gmail.com>
2019-05-09 10:41:51 +0700

    init sc

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PaddleController : NetworkBehaviour {
+
+    public float batasAtas;
+    public float batasBawah;
+
+    public float kecepatan;
+    public string axis;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        float gerak = GetInputPC();
+
+        float nextPos = transform.position.y + gerak;
+        if (nextPos > batasAtas)
+        {
+            gerak = 0;
+        }
+        if (nextPos < batasBawah)
+        {
+            gerak = 0;
+        }
+
+        transform.Translate(0, gerak, 0);
+
+        
+    }
+
+    float GetInputPC()
+    {
+        return Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+    }
+
+    private void Awake()
+    {
+        if(transform.position.x > 0)
+        {
+            transform.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            transform.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+    }
+}
